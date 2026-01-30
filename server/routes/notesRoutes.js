f20fd9e2c354a8656d98ca44a8db88bca6360c21
Lav Kushwaha <lavkumar1224@gmail.com>
2026-01-30 10:59:56 +0530

    notes routes

--- server/routes/notesRoutes.js
+++ server/routes/notesRoutes.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const { createNotes, getAllNotes, getNoteById, updateNotes, deleteNotes } = require('../controller/notesController');
-const { userAuth } = require('../middleware/userAuth');
+const express = require("express");
+const {
+  createNotes,
+  getAllNotes,
+  getNoteById,
+  updateNotes,
+  deleteNotes
+} = require("../controller/notesController");
+
+const { userAuth } = require("../middleware/userAuth");
 
 const router = express.Router();
+// router.use(userAuth);
 
-router.post("/create",userAuth,createNotes);
-router.get("/get",userAuth,getAllNotes);
-router.get("/get/:id",userAuth,getNoteById);
-router.put("/update/:id",userAuth,updateNotes);
-router.delete("/delete/:id",userAuth,deleteNotes);
+router.post("/", userAuth, createNotes);
+router.get("/", userAuth, getAllNotes);
+router.get("/:id", userAuth, getNoteById);
+router.put("/:id", userAuth, updateNotes);
+router.delete("/:id", userAuth, deleteNotes);
 
-
-module.exports = router
+module.exports = router;
